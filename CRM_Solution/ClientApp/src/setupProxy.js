5472cbad98a1ee5c9ae8776ec6a573bf68dc9a61
Stan S <stan.shm@gmail.com>
2023-06-25 20:26:48 -0500

    even more changes

--- CRM_Solution/ClientApp/src/setupProxy.js
+++ CRM_Solution/ClientApp/src/setupProxy.js
@@ -1,13 +1,13 @@
 const createProxyMiddleware = require('http-proxy-middleware');
 const { env } = require('process');
 
-//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-//    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:10932';
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:10932';
 
-const target = 'http://localhost:10932';
+
 
 const context =  [
- /* "/weatherforecast",*/
+  "/weatherforecast",
   "/_configuration",
   "/.well-known",
   "/Identity",
