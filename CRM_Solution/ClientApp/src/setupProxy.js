73cc771be59cd87728d21089cf8018d39cd0fb1e
Stan S <stan.shm@gmail.com>
2023-06-26 19:53:23 -0500

    attempt to fix react app not connecting to the back end

--- CRM_Solution/ClientApp/src/setupProxy.js
+++ CRM_Solution/ClientApp/src/setupProxy.js
@@ -1,9 +1,12 @@
 const createProxyMiddleware = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:10932';
+//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:10932';
 
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7046;http://localhost:5046';
 
 
 const context =  [
