29aed6f28e6851a5e73cecb88ae48f082477eceb
Stan S <stan.shm@gmail.com>
2023-06-27 08:50:32 -0500

    chnage in OIDC config controller

--- CRM_Solution/Controllers/OidcConfigurationController.cs
+++ CRM_Solution/Controllers/OidcConfigurationController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRM_Solution.Controllers
 {
+    [AllowAnonymous]
     public class OidcConfigurationController : Controller
     {
         private readonly ILogger<OidcConfigurationController> _logger;
